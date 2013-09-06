8344e2f72dc9c7672e95487dbc5de561bb074932
Mark Hatton <mark@zeebox.com>
2013-09-06 14:42:16 +0100

    tidy

--- app/services/AnagramSolver.scala
+++ app/services/AnagramSolver.scala
@@ -9,18 +9,20 @@ import scala.annotation.tailrec
 
 class AnagramSolver(_dictionary: Set[String]) {
 
-  private final val trie = new PatriciaTrie[String, Unit](StringKeyAnalyzer.INSTANCE)
+  private final val trie = {
+    val t = new PatriciaTrie[String, Unit](StringKeyAnalyzer.INSTANCE)
+    _dictionary.map(_.toLowerCase()) foreach (s => t.put(s, ()))
+    t
+  }
 
-  _dictionary.map(_.toLowerCase()) foreach (s => trie.put(s, ()))
+  @inline
+  private final def dictionary(s: String) = trie.containsKey(s)
 
-  private final def dictionary(s: String) = trie.select(s).getKey == s
-
+  @inline
   private final def prefix(s: String) = trie.select(s).getKey.startsWith(s)
 
-  def solve(s: String, limit: Int = 1000, timeoutMillis: Int = 5000): List[String] = {
-    val chars = s.toLowerCase().toList.filter { c =>
-      c >= 'a' && c <= 'z'
-    }
+  def solve(s: String, limit: Int = 1000, timeoutMillis: Int = 10000): List[String] = {
+    val chars = s.toLowerCase().toList.filter { c => c >= 'a' && c <= 'z'}
 
     val frontier = mutable.PriorityQueue[(String, List[String], List[Char])]()(Ordering.by{case (w, ws, av) => -ws.length})
     frontier += (("", Nil, chars))
@@ -30,32 +32,32 @@ class AnagramSolver(_dictionary: Set[String]) {
 
   @tailrec
   private final def solve(acc: mutable.ListBuffer[String], frontier: mutable.PriorityQueue[(String, List[String], List[Char])], limit: Int, timeoutAtMillis: Long): List[String] =
-    if (frontier.isEmpty) {
+    if (frontier.isEmpty)
       acc.toList
-    } else {
+    else {
       val (w, ws, avail) = frontier.dequeue()
-      avail match {
-        case Nil =>
-          val solution = (w :: ws).reverse.mkString(" ")
-          if (dictionary(w) && !acc.contains(solution)) {
-            acc += solution
-            if (acc.length > limit) return acc.toList
-          }
-        case _ =>
-          for (c: Char <- avail.toSet) yield {
-            val s = w + c
-            lazy val remain = avail diff List(c)
 
-            if (dictionary(s))
-              frontier ++= Seq(("", s :: ws, remain), (s, ws, remain))
-            else if (prefix(s))
-              frontier += ((s, ws, remain))
-          }
+      if (avail.isEmpty) {
+        lazy val solution = (w :: ws).reverse.mkString(" ")
+        if (dictionary(w) && !acc.contains(solution)) {
+          acc += solution
+          if (acc.length > limit) return acc.toList
+        }
+      } else {
+        for (c: Char <- avail.toSet) {
+          val s = w + c
+          lazy val remain = avail diff List(c)
 
-          if (frontier.length % 50 == 0) // only check clock every ~50 iterations
-            if (System.currentTimeMillis() > timeoutAtMillis) return acc.toList
+          if (dictionary(s))
+            frontier += (("", s :: ws, remain))
+          if (prefix(s))
+            frontier += ((s, ws, remain))
+        }
 
+        if (frontier.length % 1000 == 0 // only check clock every ~1000 iterations
+          && System.currentTimeMillis() > timeoutAtMillis) return acc.toList
       }
+
       solve(acc, frontier, limit, timeoutAtMillis)
     }
 
