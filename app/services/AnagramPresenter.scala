4481c24a32e4e00b4e17d23b2aacdb3324b1c995
Mark Hatton <mark@zeebox.com>
2013-09-09 22:32:26 +0100

    rank by raw bigrams score

--- app/services/AnagramPresenter.scala
+++ app/services/AnagramPresenter.scala
@@ -11,15 +11,16 @@ class AnagramPresenter(unigrams: BinarySearchCSV, bigrams: BinarySearchCSV) {
     ss.length match {
       case 1 =>
         (s, unigrams.find(s).getOrElse(1L))
-      case 2 =>
-        (s, bigrams.find(s).getOrElse(1L))
+
       case _ =>
         val max = ss.permutations.toList.map{ p =>
           val score = p.sliding(2).map(pair => bigrams.find(pair.mkString(" ")).getOrElse(1L)).min
           (p, score)
         }.sortBy{ case (_, score) => score }.last
 
-      (max._1.mkString(" "), max._2 / math.pow(ss.length, 4).toLong)
+      (max._1.mkString(" "), max._2 / (if (ss.length <= 2) 1L
+                                       else math.pow(ss.length, 4).toLong)
+      )
     }
   }
 
